b28bc57e01c0ed48ee9282e7d923e21894db123a
Gustavo Gonzalez <gustavogonzalezmejia@gmail.com>
2022-11-27 12:26:45 -0500

    Botón ver documentos creado y con colores según figma

--- RetoSP/ViewControlers/MenuScreenViewController.swift
+++ RetoSP/ViewControlers/MenuScreenViewController.swift
@@ -9,11 +9,19 @@ import UIKit
 
 class MenuScreenViewController: UIViewController {
 
+    @IBOutlet weak var boton1: UIButton!
+    
+    @IBOutlet weak var enviarDocumentosScrollView: UIScrollView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.navigationItem.setHidesBackButton(true, animated: true)
-       
+        setupUI()
     }
     
-
+    func setupUI(){
+        self.navigationItem.setHidesBackButton(true, animated: true)
+        self.enviarDocumentosScrollView.layer.cornerRadius = 10.0
+        self.enviarDocumentosScrollView.layer.borderColor = UIColor(named: "enviarDocumentos")?.cgColor
+        self.enviarDocumentosScrollView.layer.borderWidth = 1.0
+    }
 }
