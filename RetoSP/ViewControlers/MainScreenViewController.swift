7597b068c12ec0b32424df648410ccc4e19a1886
Gustavo Gonzalez <gustavogonzalezmejia@gmail.com>
2022-11-26 19:57:30 -0500

    Se crearon los enlaces entre la vista y el view controller.

--- RetoSP/ViewControlers/MainScreenViewController.swift
+++ RetoSP/ViewControlers/MainScreenViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainScreenViewController.swift
 //  RetoSP
 //
 //  Created by Usuario on 26/11/22.
@@ -7,15 +7,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainScreenViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-
-
+       
     }
-
+    
 
 }
-
---
+++ RetoSP/ViewControlers/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  RetoSP
+//
+//  Created by Usuario on 26/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var logoImage: UIImageView!
+    
+    @IBOutlet weak var label: UILabel!
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var loginButton: UIButton!
+    
+    @IBOutlet weak var biometricButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+    @IBAction func loginButtonTapped(_ sender: Any) {
+       
+    }
+    
+    @IBAction func biometricButtonTapped(_ sender: Any) {
+    }
+    
+    
+
+
+}
+
