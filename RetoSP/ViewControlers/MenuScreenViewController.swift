99e7aacd4fefe8a828edd7cb7b9daabf7d23c0bb
Gustavo Gonzalez <gustavogonzalezmejia@gmail.com>
2022-11-29 12:17:04 -0500

    Se Agregaron los 3 botones de Menu Screen

--- RetoSP/ViewControlers/MenuScreenViewController.swift
+++ RetoSP/ViewControlers/MenuScreenViewController.swift
@@ -13,7 +13,9 @@ class MenuScreenViewController: UIViewController {
     
     @IBOutlet weak var enviarDocumentosScrollView: UIScrollView!
     
+    @IBOutlet weak var verDocumentosScrollView: UIScrollView!
     
+    @IBOutlet weak var verOficinasScrollView: UIScrollView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,11 +28,13 @@ class MenuScreenViewController: UIViewController {
         self.enviarDocumentosScrollView.layer.borderColor = UIColor(named: "enviarDocumentos")?.cgColor
         self.enviarDocumentosScrollView.layer.borderWidth = 1.0
         
-        self.enviarDocumentosScrollView.layer.cornerRadius = 10.0
-        self.enviarDocumentosScrollView.layer.borderColor = UIColor(named: "enviarDocumentos")?.cgColor
-        self.enviarDocumentosScrollView.layer.borderWidth = 1.0
-        
-        
+        self.verDocumentosScrollView.layer.cornerRadius = 10.0
+        self.verDocumentosScrollView.layer.borderColor = UIColor(named: "verDocumentos")?.cgColor
+        self.verDocumentosScrollView.layer.borderWidth = 1.0
+
+        self.verOficinasScrollView.layer.cornerRadius = 10.0
+        self.verOficinasScrollView.layer.borderColor = UIColor(named: "verOficinas")?.cgColor
+        self.verOficinasScrollView.layer.borderWidth = 1.0
 
     }
 }
